df0124c1e7d092388d29204a257984b708e9a084
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-03-27 08:01:30 +0800

    remove debug line

--- test/test_suite/gen.clj
+++ test/test_suite/gen.clj
@@ -5,8 +5,6 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.generators :as gen]))
 
-(t/check-form* `'~(gen/sample gen/any-printable))
-
 (def check-constants
     (prop/for-all [v (gen/vector gen/any-printable)]
                   (do (t/check-form* `(quote ~v))
